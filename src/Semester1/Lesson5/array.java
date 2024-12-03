91fe59a0906b3aa8b5538158244c4a0402554fe3
Nehorai0258 <nehorai0258@gmail.com>
2024-12-04 00:06:24 +0200

    regular

--- src/Semester1/Lesson5/array.java
+++ src/Semester1/Lesson5/array.java
@@ -279,7 +279,7 @@ class Ex10and11 {
 class Ex12{
     public static void printArray(int[] arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i] + " ,");
         }
     }
 
@@ -288,15 +288,168 @@ class Ex12{
         return arr;
     }
 
-//    public static int[] createRandomArray(int num){
-//        Random rnd = new Random();
-//        int[] arr = creatArray(num);
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = rnd.nextInt(num*10);
-//        }
-//    }
+    public static int[] createRandomArray(int num){
+        Random rnd = new Random();
+        int[] arr = creatArray(num);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(num*10);
+        }
+        return arr;
+    }
+
+    public static int mostCommonNumInArray(int[] arr){
+        int mostCommon = arr[0];
+        int maxCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int currentNumberCounter = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]){
+                    currentNumberCounter++;
+                }
+
+            }
+            if (currentNumberCounter > maxCount){
+                mostCommon = arr[i];
+                maxCount = currentNumberCounter;
+            }
+        }
+        return mostCommon;
+    }
+
+    public static int[] arrayWithoutCommonNum(int commonNum, int[] arr){
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == commonNum){
+                counter++;
+            }
+        }
+        int[] newArr = new int[arr.length - counter];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != commonNum){
+                newArr[index] = arr[i];
+                index++;
+            }
+        }
+        return newArr;
+    }
 
     public static void main(String[] args) {
+        int arrSize = 7;
+        int[] arr = createRandomArray(arrSize);
+        printArray(arr);
 
+        System.out.println();
+        int mostCommonNum = mostCommonNumInArray(arr);
+        System.out.println("Most common number is: " + mostCommonNum);
+
+        System.out.println();
+        int[] newArr = arrayWithoutCommonNum(mostCommonNum, arr);
+        System.out.println("New array:");
+        printArray(newArr);
+
+    }
+}
+
+class Ex13{
+    public static int sumOfBiggerArray(int [] arr1, int[] arr2){
+        int sumOfArray1 = 0;
+        int sumOfArray2 = 0;
+        for (int i : arr1){
+            sumOfArray1 += i;
+        }
+        for (int j : arr2){
+            sumOfArray2 += j;
+        }
+        if (sumOfArray1 == sumOfArray2){
+            return 0;
+        } else if (sumOfArray1 > sumOfArray2) {
+            return 1;
+        }
+        return 2;
+    }
+
+    public static void main(String[] args) {
+        int num = 10;
+
+        int[] arr1 = Ex12.createRandomArray(num);
+        System.out.println();
+        int[] arr2 = Ex12.createRandomArray(num);
+        System.out.println();
+
+        Ex12.printArray(arr1);
+        System.out.println();
+        Ex12.printArray(arr2);
+        System.out.println();
+
+        System.out.println(sumOfBiggerArray(arr1, arr2));
+    }
+}
+
+class Ex14{
+    public static int[] creatingArrOfTheNum(int num){
+        int counter = 0;
+        int tempNum = num;
+        while (tempNum != 0){
+            counter++;
+            tempNum /= 10;
+        }
+        int[] arr = new int[counter];
+        for (int i = arr.length-1; i >= 0; i--) {
+            arr[i] = num % 10;
+            num /= 10;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int num = 12387665;
+        int[] arr = creatingArrOfTheNum(num);
+        Ex12.printArray(arr);
+    }
+}
+
+class Ex15{
+    public static boolean isPalindromicArray(int[] arr1){
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr1[arr1.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,0,5,0,4,3,2,1};
+        System.out.println(isPalindromicArray(arr1));
+    }
+}
+
+class Ex16{
+
+    public static void main(String[] args) {
+       int num = 168161;
+       int[] arr = Ex14.creatingArrOfTheNum(num);
+        System.out.println(Ex15.isPalindromicArray(arr));
+    }
+}
+
+class Ex17{
+    public static int counterOfPolindromNumbers(int[] arr){
+        int counter = 0;
+        for (int i : arr){
+            int[] tempArr = Ex14.creatingArrOfTheNum(i);
+            if (Ex15.isPalindromicArray(tempArr)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {181, 777, 889, 16361 , 151 , 1981 , 18584 , 1581512};
+        System.out.println(counterOfPolindromNumbers(arr));
     }
 }
